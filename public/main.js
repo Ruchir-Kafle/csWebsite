6ddd95bcf0df27446a0b01fb329a35ad8e926d99
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2025-08-29 09:45:37 -0400

    home page

--- public/main.js
+++ public/main.js
@@ -1,10 +1,23 @@
 const body = document.body;
 
 function addConsole() {
+    const homeConsoleDiv = document.querySelector(`.home-console`);
+    const indexConsoleDiv = document.querySelector(`.index-console`);
+    let directory = "./home.html";
+    let consoleText = "cd .."
+
+    if (homeConsoleDiv != null) {
+        directory = "../index.html";
+    } else if (indexConsoleDiv != null) {
+        directory = "pages/home.html";
+        consoleText = "cd home.html";
+    }
+
+
     body.innerHTML += 
     `<div class="console">
-        <a href="./home.html">
-            $ <span class="command">cd ..</span>
+        <a href="${directory}">
+            $ <span class="command">${consoleText}</span>
         </a>
     </div>`;
 }
