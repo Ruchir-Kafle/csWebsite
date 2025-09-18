0113cfb0521d824eba8cfa3b2ecbe9cd62410ed7
Ruchir <ruchirkafle@gmail.com>
2025-09-17 20:25:20 -0400

    polishes

--- public/main.js
+++ public/main.js
@@ -1,13 +1,30 @@
-async function addConsole(body, consoleResolve) {
-    const delayPerLetter = 100;
+const delayPerLetter = 100;
 
+const blinkerIntervalMilliseconds = 500;
+
+const characterList = `@#$%^&*()_+-=[{}]'";:.?`;
+const defaultCharactersPerLine = 40;
+const potentialCharactersPerLine = 40;
+
+const arrowCount = 50;
+const delayPerLine = 50;
+
+const terminalArrowCount = arrowCount * 1.5;
+
+const directoryPointer = `<--- current file`;
+
+const delayBeforeStart = 1000;
+
+
+async function addConsole(body, consoleResolve) {
     let directory = "../index.html";
-    let consoleText = "..";
-    let command = "cd ";
+    let consoleText = "out";
+    let command = "sign ";
 
     if (document.title == "ruchir_kafle's_website") {
-        directory = "pages/home.html";
-        consoleText = "./home/";
+        command = "enter ";
+        directory = "pages/terminal.html";
+        consoleText = "ruchir's_system";
     }
 
     body.innerHTML += 
@@ -36,8 +53,6 @@ function typeWriter(element, text, delayPerLetter, typeWriterResolve) {
 
 
 function consoleBlinker() {
-    const blinkerIntervalMilliseconds = 500;
-
     const blinker = `<span class="blinker">|</span>`;
     const originalConsoleInnerHTML = document.querySelector(`.console .command`).innerHTML;
     
@@ -59,11 +74,9 @@ function consoleBlinker() {
 
 
 function lineOfText() {
-    const characterList = `@#$%^&*()_+-=[{}]'";:.?`;
-    
     let currentLine = document.createElement("p");
 
-    for (let i = 0; i < Math.floor( 40 + Math.random() * 40 ); i++) {
+    for (let i = 0; i < Math.floor( defaultCharactersPerLine + Math.random() * potentialCharactersPerLine ); i++) {
         let randomCharacter = characterList[Math.floor( Math.random() * characterList.length )];
         currentLine.innerHTML += randomCharacter;
     }
@@ -88,18 +101,16 @@ function staggeredGenerator(count, max, textTimeoutMilliseconds, action, resolve
 
 
 async function wallOfText(main, wallResolve) {
-    const delayPerLine = 50;
-
     main.innerHTML += `<div class="wall-of-text"></div>`;
     const wallDiv = document.querySelector(`.wall-of-text`);
 
     let lines = [
         5,
-        "Hack into Ruchir's system",
+        "Hack into Ruchir's system,",
         5,
-        "Go to bottom of page",
+        "Go to bottom of page,",
         6,
-        "Click on the command in the terminal at the bottom",
+        "Click on the yellow text.",
         7
     ]
 
@@ -110,9 +121,20 @@ async function wallOfText(main, wallResolve) {
                 wallDiv.appendChild(lineOfText())
             }, linesResolve));
         } else {
-            let currentLine = document.createElement("p");
+            const fillerLinesCount = 1;
+            
+            let currentLine = document.createElement("div");
             currentLine.classList.add("important-line");
-            currentLine.innerHTML = line;
+
+            for (let i = 0; i < fillerLinesCount * 2 + 1; i++) {
+                let save;
+                if (i < fillerLinesCount) save = `<p>` + `v`.repeat(arrowCount) + `</p>`;
+                else if (i == fillerLinesCount) save = `<p>${line}</p>`;
+                else if (i > fillerLinesCount) save = `<p>` + `^`.repeat(arrowCount) + `</p>`;
+
+                currentLine.innerHTML += save;
+            }
+
             wallDiv.appendChild(currentLine);
         }
 
@@ -126,21 +148,34 @@ async function wallOfText(main, wallResolve) {
 function topConsole(navigationNode, file) {
     const console = document.createElement("p");
     console.classList.add("top-console");
-    console.innerHTML = `<span class="user">root@ruchir:~$</span> <span class="command">cat ${file} </span>`;
+    console.innerHTML = `<span class="user">root@ruchir:~$</span> <span class="command">read ${file} </span>`;
     
     navigationNode.insertAdjacentElement("afterend", console);
 }
 
 
-async function start(main, body) {
-    const delay = 1000;
+function preProcessing(main) {
 
+    if (main.children.length <= 1 && document.title != "terminal")
+           main.innerHTML += `<div class="no-contents-error"><p>404: FILE NOT FOUND.</p> <p>Please try again when Ruchir has put content in this file.</p></div>`;
+
+    if (document.title == "terminal")
+        main.innerHTML += `
+        <div class="important-line">
+            ${`<p>` + `v`.repeat(terminalArrowCount) + `</p>`}
+            <p>Select a file to view (in <span class="green">green</span>) from the navigation bar above.</p>
+            ${`<p>` + `^`.repeat(terminalArrowCount) + `</p>`}
+        </div>
+        `;
+}
+
+
+async function start(main, body) {
     if (document.title == "ruchir_kafle's_website") {
-        await new Promise(delayResolve => setTimeout(() => delayResolve(), delay))
+        await new Promise(delayResolve => setTimeout(() => delayResolve(), delayBeforeStart))
         await new Promise(wallResolve => wallOfText(main, wallResolve));
     } else {
-        if (main.children.length <= 1 && document.title != "terminal")
-           main.innerHTML += `<div class="no-contents-error"><p>404: FILE NOT FOUND.</p> <p>Please try again when Ruchir has made this file.</p></div>`;
+        preProcessing(main);
 
         let navBar = document.createElement("div");
         navBar.classList.add("navigation");
@@ -148,17 +183,18 @@ async function start(main, body) {
         navBar.innerHTML =
         `
         <p>
-            <span class="user">root@ruchir:~$</span> <span class="command">ls</span>
+            <span class="user">root@ruchir:~$</span> <span class="command">show files</span>
         </p>
         <nav class="navigation-bar">
-            <li><span>├────</span><a href="./about.html">about.html</a> <- link</li>
-            <li><span>├────</span><a href="./cs.html">cs.html</a></li>
-            <li><span>├────</span><a href="./stemOne.html">stem_one.html</a></li>
-            <li><span>├────</span><a href="./stemTwo.html">stem_two.html</a></li>
-            <li><span>├────</span><a href="./physics.html">physics.html</a></li>
-            <li><span>├────</span><a href="./spanish.html">spanish.html</a></li>
-            <li><span>├────</span><a href="./math.html">math.html</a></li>
-            <li><span>└────</span><a href="./humanities.html">humanities.html</a></li>
+            <li><span>├────</span><a href="./terminal.html">terminal</a> ${"terminal" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./about.html">about.html</a> ${"about.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./cs.html">cs.html</a> ${"cs.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./stemOne.html">stem_one.html</a> ${"stem_one.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./stemTwo.html">stem_two.html</a> ${"stem_two.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./physics.html">physics.html</a> ${"physics.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./spanish.html">spanish.html</a> ${"spanish.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>├────</span><a href="./math.html">math.html</a> ${"math.html" == document.title ? directoryPointer : ""}</li>
+            <li><span>└────</span><a href="./humanities.html">humanities.html</a> ${"humanities.html" == document.title ? directoryPointer : ""}</li>
         </nav>
         `;
     
